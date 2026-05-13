d74d3fb51012936f6adb124029f4b84dea662de3
Brendan Tang <brendantang616@gmail.com>
2026-05-13 04:28:34 -0400

    wrote some print statements and took user inputs

---
+++ adventure.py
@@ -0,0 +1,6 @@
+print("Welcome to the Choose Your Own Adventure Game!")
+
+name = input("What is your name? ")
+age = input("What is your age? ")
+
+print("Hello", name, "Your age is", age, "years old.")
