dcbbe6c47eb32676886eeb6e31dc28254976f63f
Brendan Tang <brendantang616@gmail.com>
2026-05-15 19:11:26 -0400

    added if/else statements

--- adventure.py
+++ adventure.py
@@ -1,6 +1,16 @@
 print("Welcome to the Choose Your Own Adventure Game!")
 
 name = input("What is your name? ")
-age = input("What is your age? ")
+age = int(input("What is your age? "))
 
 print("Hello", name, "Your age is", age, "years old.")
+
+if age >= 18:
+    print("You are old enough!")
+
+    ans = input("Do you want to play? ")
+    if ans == "yes":
+        print("Let's play!")
+
+else:
+    print("You are not old enough to play...")
